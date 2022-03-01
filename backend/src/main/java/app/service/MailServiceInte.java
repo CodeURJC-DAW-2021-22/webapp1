d5b8365afc0029ea6b2b5ccd6c5bbb6012db7e01
HectorURJC <hec20tor0@gmail.com>
2022-03-01 12:16:05 +0100

    First_Attempt

---
+++ backend/src/main/java/app/service/MailServiceInte.java
@@ -0,0 +1,11 @@
+/*package app.service;
+ 
+import java.io.File;
+ 
+public interface MailServiceInte {
+ 
+	public void send(String to, String subject, String text);
+	
+	public void send(String to, String subject, String text, File... attachments);
+ 
+}*/
