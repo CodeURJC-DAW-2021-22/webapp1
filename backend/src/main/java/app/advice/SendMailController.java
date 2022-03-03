ea33c691a0d0ff7654b277387b5adaac3c8c3d33
HectorURJC <hec20tor0@gmail.com>
2022-03-03 12:01:16 +0100

    Update SendMailController.java

--- backend/src/main/java/app/advice/SendMailController.java
+++ backend/src/main/java/app/advice/SendMailController.java
@@ -4,14 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import app.service.MessageAutomatic;
+import app.service.messageAutomatic;
 
 
 @RestController
 public class SendMailController {
 
     @Autowired
-    private MessageAutomatic mailService;
+    private messageAutomatic mailService;
 
     @PostMapping
     public String sendMail(String nameFilm, String from){
