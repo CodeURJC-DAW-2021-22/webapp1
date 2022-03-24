581523e562b1227a2a7471d1d2269927da1eeefc
Vanesa <v.reina.2019@alumnos.urjc.es>
2022-03-24 20:42:11 +0100

    DownloadImageProfile function

--- backend/src/main/java/app/controller/UserRestController.java
+++ backend/src/main/java/app/controller/UserRestController.java
@@ -1,12 +1,30 @@
 package app.controller;
 
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.hibernate.engine.jdbc.BlobProxy;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import app.model.User;
 import app.service.CommentService;
@@ -35,5 +53,16 @@ public class UserRestController {
 		} else {
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 		}
+	}
+	
+	@GetMapping("/{id}/imageProfile")
+	public ResponseEntity<Object> downloadImageProfile(@PathVariable long id) throws SQLException {
+		Optional<User> user = userService.findById(id);
+	    if (user.isPresent() && user.get().getImageFile() != null) {
+	        Resource file = new InputStreamResource(user.get().getImageFile().getBinaryStream());
+	        return ResponseEntity.ok().header(HttpHeaders.CONTENT_TYPE, "image/jpeg").contentLength(user.get().getImageFile().length()).body(file);
+	    } else {
+	        return ResponseEntity.notFound().build();
+	    }
 	}
 }
