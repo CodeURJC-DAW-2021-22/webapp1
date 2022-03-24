e53b2a97465a0593d184ad9e3d0e3f354fdc3efa
migue <m.regato.2019@alumnos.urjc.es>
2022-03-24 21:16:04 +0100

    UserRestController many functions

--- backend/src/main/java/app/controller/UserRestController.java
+++ backend/src/main/java/app/controller/UserRestController.java
@@ -11,7 +11,6 @@ import org.hibernate.engine.jdbc.BlobProxy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,7 +18,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -27,8 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import app.model.User;
-import app.service.CommentService;
-import app.service.FilmService;
+import app.modelRest.ListFollowUser;
 import app.service.UserService;
 
 @RestController
@@ -39,10 +36,7 @@ public class UserRestController {
 	private UserService userService;
 	
 	@Autowired
-	private FilmService filmService;
-	
-	@Autowired
-	private CommentService commentService;
+	private PasswordEncoder passwordEncoder;
 	
 	@GetMapping("/{id}")
 	public ResponseEntity<User> getProfile(@PathVariable long id) {
@@ -74,5 +68,88 @@ public class UserRestController {
 			return new ResponseEntity<>(user, HttpStatus.OK);
 		}
 		return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+	}
+	
+	@PutMapping("/editPassword")
+	public ResponseEntity<User> editPasswordProcess(Model model, @RequestParam long id, @RequestParam String oldPassword, @RequestParam String newPassword) throws IOException, SQLException {
+		User user = userService.findById(id).orElseThrow(); 
+		
+		if (passwordEncoder.matches(oldPassword, user.getEncodedPassword())){
+			user.setEncodedPassword(passwordEncoder.encode(newPassword));
+			userService.save(user);
+			return new ResponseEntity<>(user, HttpStatus.OK);
+		}
+		
+		return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+	}
+	
+	@GetMapping("/followers/{id}")
+	public ResponseEntity<ListFollowUser> followers(Model model, @PathVariable long id, HttpServletRequest request) {
+		User user = userService.findByName(request.getUserPrincipal().getName()).orElseThrow();
+		List<User> followers = user.getFollowers();
+		ListFollowUser listFollowersUser = new ListFollowUser();
+		listFollowersUser.setUser(user);
+		listFollowersUser.setFollowers(followers);
+		
+		return new ResponseEntity<>(listFollowersUser, HttpStatus.OK);
+	}
+	
+	@GetMapping("/following/{id}")
+	public ResponseEntity<ListFollowUser> following(Model model, @PathVariable long id, HttpServletRequest request) {		
+		User user = userService.findByName(request.getUserPrincipal().getName()).orElseThrow();
+		List<User> following = user.getFollowing();
+		ListFollowUser listFollowingUser = new ListFollowUser();
+		listFollowingUser.setUser(user);
+		listFollowingUser.setFollowers(following);
+		
+		return new ResponseEntity<>(listFollowingUser, HttpStatus.OK);
+	}
+	
+	@GetMapping("/watchProfile/{id}")
+	public ResponseEntity<User> watchProfile(Model model, @PathVariable long id, HttpServletRequest request) {
+		User follower = userService.findByName(request.getUserPrincipal().getName()).orElseThrow();
+		User following = userService.findById(id).orElseThrow();
+		
+		if (!follower.getId().equals(following.getId())) {
+			
+			return new ResponseEntity<>(following, HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(follower, HttpStatus.OK);
+		}
+	}
+	
+	@GetMapping("/followUnfollow/{id}")
+	public ResponseEntity<User> followUnfollow(Model model, @PathVariable long id, HttpServletRequest request) {
+		User follower = userService.findByName(request.getUserPrincipal().getName()).orElseThrow();
+		User following = userService.findById(id).orElseThrow();
+		
+		if(!follower.getFollowing().contains(following)) {
+			follower.addFollowing(following);
+		} else {
+			follower.deleteFollowing(following);
+		}
+		
+		follower.calculateFollowers();
+		follower.calculateFollowing();
+				
+		following.calculateFollowers();
+		following.calculateFollowing();
+		
+		userService.save(follower);
+		return new ResponseEntity<>(following, HttpStatus.OK);
+	}
+	
+	private void updateImageProfile(User user, MultipartFile imageField) throws IOException, SQLException {
+		if (!imageField.isEmpty()) {
+			user.setImageFile(BlobProxy.generateProxy(imageField.getInputStream(), imageField.getSize()));
+			user.setImage(true);
+		} else {
+			User dbUser = userService.findById(user.getId()).orElseThrow();
+			
+			if (dbUser.getImage()) {
+				user.setImageFile(BlobProxy.generateProxy(dbUser.getImageFile().getBinaryStream(), dbUser.getImageFile().length()));
+				user.setImage(true);
+			}
+		}
 	}
 }
