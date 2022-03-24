705fff16e44f7855d32522a4d27a2310c41a1173
migue <m.regato.2019@alumnos.urjc.es>
2022-03-24 12:06:30 +0100

    GetProfile function added to UserRestController

---
+++ backend/src/main/java/app/controller/UserRestController.java
@@ -0,0 +1,39 @@
+package app.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import app.model.User;
+import app.service.CommentService;
+import app.service.FilmService;
+import app.service.UserService;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserRestController {
+	
+	@Autowired
+	private UserService userService;
+	
+	@Autowired
+	private FilmService filmService;
+	
+	@Autowired
+	private CommentService commentService;
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<User> getProfile(@PathVariable long id) {
+		User user = userService.findById(id).orElseThrow();
+		
+		if (user != null) {
+			return new ResponseEntity<>(user, HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
+}
