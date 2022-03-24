b9321c75e1d73ce6e27395d5a498324f50619f64
HectorURJC <hec20tor0@gmail.com>
2022-03-24 23:22:30 +0100

    Rename_Class

---
+++ backend/src/main/java/app/security/RestSecurityConfig.java
@@ -0,0 +1,64 @@
+package app.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+//import es.codeurjc.daw.library.security.jwt.JwtRequestFilter;
+
+
+@Configuration
+@Order(1)
+public class RestSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+    RepositoryUserDetailsService userDetailsService;
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
+	//@Autowired
+	//private JwtRequestFilter jwtRequestFilter;
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.antMatcher("/api/**");
+		
+		// URLs that need authentication to access to it
+		http.authorizeRequests().antMatchers(HttpMethod.POST, "/api/**").hasRole("USER");
+		http.authorizeRequests().antMatchers(HttpMethod.PUT, "/api/**").hasRole("USER");
+		http.authorizeRequests().antMatchers(HttpMethod.PUT, "/api/**").hasRole("ADMIN");
+		http.authorizeRequests().antMatchers(HttpMethod.POST, "/api/**").hasRole("ADMIN");
+		http.authorizeRequests().antMatchers(HttpMethod.DELETE, "/api/**").hasRole("ADMIN");
+		
+		// Other URLs can be accessed without authentication
+		http.authorizeRequests().anyRequest().permitAll();
+
+		// Disable CSRF protection (it is difficult to implement in REST APIs)
+		http.csrf().disable();
+
+		// Disable Http Basic Authentication
+		http.httpBasic().disable();
+		
+		// Disable Form login Authentication
+		http.formLogin().disable();
+
+		// Avoid creating session 
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		
+		// Add JWT Token filter
+		//http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+	}
+}
