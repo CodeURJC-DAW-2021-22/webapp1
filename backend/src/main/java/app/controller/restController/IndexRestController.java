38e14d93976f37cf41340b72e7b6a4ed40262793
HectorURJC <hec20tor0@gmail.com>
2022-03-28 11:32:44 +0200

    IndexRestController

---
+++ backend/src/main/java/app/controller/restController/IndexRestController.java
@@ -0,0 +1,10 @@
+package app.controller.restController;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+public class IndexRestController {
+	
+}
