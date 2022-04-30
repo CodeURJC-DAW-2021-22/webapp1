6b3ec1da7b8d99fcbb3e539f0c985aaa0f08412a
migue <m.regato.2019@alumnos.urjc.es>
2022-04-30 13:25:43 +0200

    downloadImage, editProfile and editImage

--- frontend/src/app/services/user.service.ts
+++ frontend/src/app/services/user.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
+import { catchError, Observable, throwError } from "rxjs";
 import { UserComments } from "../models/rest/userComments.model";
+import { User } from "../models/user.model";
 
 const BASE_URL = '/api/users';
 
@@ -10,7 +11,7 @@ export class UserService {
 
     constructor(private httpClient: HttpClient) { }
 
-    getUser(id: number): Observable<UserComments>{
+    getUser(id: number): Observable<UserComments> {
         return this.httpClient.get(BASE_URL + '/' + id).pipe(
         ) as Observable<UserComments>;
     }
@@ -23,4 +24,46 @@ export class UserService {
     register(formData: FormData) {
         return this.httpClient.post(BASE_URL + "/", formData, { withCredentials: true });
     }
+
+    downloadImage(user: User) {
+        return user.image? 'api/users/' + user.id + '/image' : 'assets/images/defaultImage.png';
+    }
+
+    /* moreComments(page: number) {
+        
+    } */
+
+    editProfile(user: User) {
+        if (user.id) {
+            return this.httpClient.post(BASE_URL, user)
+                .pipe(
+                    catchError(error => this.handleError(error))
+                );
+        } else {
+            return this.httpClient.put('/api/auth/login', user)
+                .pipe(
+                    catchError(error => this.handleError(error))
+                );
+        }
+    }
+
+    editImage(user: User) {
+        if (user.id) {
+            return this.httpClient.post(BASE_URL + '/' + user.id + '/image', user)
+                .pipe(
+                    catchError(error => this.handleError(error))
+                );
+        } else {
+            return this.httpClient.put('/api/auth/login', user)
+                .pipe(
+                    catchError(error => this.handleError(error))
+                );
+        }
+    }
+
+    private handleError(error: any) {
+		console.log("ERROR:");
+		console.error(error);
+		return throwError("Server error (" + error.status + "): " + error.text())
+	}
 }
