4ac5ee5e391fb6c35c4b326406d098776cc39977
migue <m.regato.2019@alumnos.urjc.es>
2022-05-01 19:18:36 +0200

    Followers Component finished

--- frontend/src/app/components/users/followers.component.ts
+++ frontend/src/app/components/users/followers.component.ts
@@ -1,19 +1,56 @@
-import { Component, OnInit } from "@angular/core";
+import { Component} from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { UserComments } from "src/app/models/rest/userComments.model";
 import { User } from "src/app/models/user.model";
+import { UserService } from "src/app/services/user.service";
 
 @Component({
     templateUrl: './followers.component.html',
     styleUrls: ['../../../assets/css/style.component.css', '../../../assets/css/loadingButton.component.css'],
 })
 
-export class FollowersComponent implements OnInit {
+export class FollowersComponent {
 
     user!: User;
-    followers: User[] = []
-    token: any;
+    userComments!: UserComments;
+    followersList: User[] = [];
     
-    ngOnInit(): void {
-        throw new Error("Method not implemented.");
+    loader: boolean = false;
+    page: number = 0;
+
+    constructor(private userService: UserService, private activatedRouter: ActivatedRoute) {
+        this.userService.getUser(activatedRouter.snapshot.params['id']).subscribe(
+            response => {
+                this.userComments = response;
+                this.user = this.userComments.user;
+                this.userService.followers(this.user.id, 0).subscribe(
+                    response => {
+                        this.followersList = response.content;
+                    }
+                )
+            }
+        );
+        
     }
+
+    followers() {
+        this.loader = true;
+        this.page += 1;
+
+        this.userService.followers(this.user.id, this.page).subscribe(
+            response => {
+                response.content.forEach(userF => {
+                    this.followersList.push(userF);
+                });
+
+                this.loader = false;
+            },
+            error =>  {
+                console.log(error);
+                this.loader = false;
+            }
+        );
+    }
+
 
 }
