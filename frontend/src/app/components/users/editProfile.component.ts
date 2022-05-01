7da96b86d3d12c0d5ff7abc7eb83922fe3ffb0a0
migue <m.regato.2019@alumnos.urjc.es>
2022-05-02 01:55:18 +0200

    Edit Profile finished

--- frontend/src/app/components/users/editProfile.component.ts
+++ frontend/src/app/components/users/editProfile.component.ts
@@ -1,17 +1,58 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, ViewChild } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+import { UserComments } from "src/app/models/rest/userComments.model";
 import { User } from "src/app/models/user.model";
+import { LoginService } from "src/app/services/login.service";
+import { UserService } from "src/app/services/user.service";
 
 @Component({
     templateUrl: './editProfile.component.html',
     styleUrls: ['../../../assets/css/style.component.css'],
 })
 
-export class EditProfile implements OnInit {
+export class EditProfile {
 
     user!: User;
-    token: any;
-    
-    ngOnInit(): void {
-        throw new Error("Method not implemented.");
+    email!: string;
+
+    @ViewChild("file")
+    file: any;
+
+    constructor(private userService: UserService, private router: Router, private loginService: LoginService) {
+
+        if (!this.loginService.isLogged()) {
+            this.router.navigate(['/login']);
+        }
+
+        this.userService.getMe().subscribe(
+            response => this.user = response.user
+        );
+    }
+
+    save() {
+        let formData = new FormData();
+        formData.append("newEmail", this.email);
+        this.userService.editProfile(this.user.id, formData).subscribe(
+            _ => this.uploadImage(),
+            _ => alert('Error updating user profile')
+        );
+    }
+
+    uploadImage() {
+        const image = this.file.nativeElement.files[0];
+        if (image) {
+          let formData = new FormData();
+          formData.append("imageFile", image);
+          this.userService.editImage(this.user.id, formData).subscribe(
+            _ => this.afterUploadImage(),
+            _ => alert('Error uploading user image: ')
+          );
+        } else {
+          this.afterUploadImage();
+        }
+    }
+
+    private afterUploadImage() {
+        this.router.navigate(['/account']);
     }
 }
