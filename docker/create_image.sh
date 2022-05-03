200f1ecb2fc7980a62cade491859bec44ac1e1d8
HectorURJC <hec20tor0@gmail.com>
2022-05-03 04:17:16 +0200

    Create_Image_Work

--- docker/create_image.sh
+++ docker/create_image.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-
+cd ../frontend
+npm install
+npm run build --prod --base-href="/new/"
+cp -r dist/AdviceMe/* ../backend/src/main/resources/public/new
+cd ../docker
 docker build -f Dockerfile -t $1 ../backend
 docker push $1
